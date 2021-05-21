28b0d0d4faa417e4c53161b63f1b3f4cd4ec6a58
szutryk <wojtekszutryk@gmail.com>
2021-05-21 12:39:59 +0200

    add Line component

--- src/components/drawElements/line/Line.js
+++ src/components/drawElements/line/Line.js
@@ -1,23 +1,62 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {useMousePosition} from "../../../hooks/useMousePosition";
 
-const Line = () => {
-    const position = useMousePosition();
-    const [selectedPoints, setSelectedPoints] = useState(0);
-    // const [position, setPosition] = useState({ x: 0, y: 0 });
-    //
-    // useEffect(() => {
-    //     const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
-    //     window.addEventListener("mousemove", setFromEvent);
-    //
-    //     return () => {
-    //         window.removeEventListener("mousemove", setFromEvent);
-    //     };
-    // }, []);
+const Line = ({color= 'red',
+                linePattern = null,
+                lineWidth = 1,
+                canvasHeight = window.innerHeight,
+                canvasWidth = window.innerWidth,}) => {
+
+    const cursorPosition = useMousePosition();
+    const pointsValue = useRef(0);
+    const [selectedPoints, setSelectedPoints] = useState(pointsValue.current);
+    const [firstPointPosition, setFirstPointPosition] = useState({ x: null, y: null });
+    const [secondPointPosition, setSecondPointPosition] = useState({ x: null, y: null });
+
+    useEffect(() => {
+        const setFromEvent = (e) => {
+            const returnFunction = (pointsValue.current === 0)
+                ? () => {
+                    pointsValue.current += 1;
+                    setSelectedPoints(pointsValue.current)
+                    setFirstPointPosition({ x: e.clientX, y: e.clientY })
+                }
+                : () => {
+                    pointsValue.current += 1;
+                    setSelectedPoints(pointsValue.current)
+                    setSecondPointPosition({ x: e.clientX, y: e.clientY })
+                }
+            if (pointsValue.current >= 2) return window.removeEventListener("click", setFromEvent);
+            return returnFunction()
+        }
+        window.addEventListener("click", setFromEvent);
+
+        return () => {
+            window.removeEventListener("click", setFromEvent);
+        };
+    }, []);
+
+    const lineStyle = {
+        stroke: color,
+        strokeWidth: lineWidth,
+        strokeDasharray: linePattern
+    };
+
+
     return (
-        <svg height={window.innerHeight} width={window.innerWidth}>
-            <line x1="0" y1="0" x2={position.x} y2={position.y} style={{stroke:"rgb(255,0,0)"}} />
-        </svg>
+        pointsValue.current > 0
+            ?
+                <svg height={canvasHeight} width={canvasWidth}>
+                    <line
+                        x1={selectedPoints === 0 ? cursorPosition.x : firstPointPosition.x}
+                        y1={selectedPoints === 0 ? cursorPosition.y : firstPointPosition.y}
+                        x2={selectedPoints === 1 ? cursorPosition.x : secondPointPosition.x}
+                        y2={selectedPoints === 1 ? cursorPosition.y : secondPointPosition.y}
+                        style={lineStyle}
+                    />
+                </svg>
+            :
+                null
     );
 };
 
